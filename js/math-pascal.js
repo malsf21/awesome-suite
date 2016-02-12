7209df43b543f4860040e212e7866b89ec2d9ce1
malsf21 <matthewang.wang@gmail.com>
2016-02-12 15:48:07 -0500

    adds math-pascal beta

---
+++ js/math-pascal.js
@@ -0,0 +1,39 @@
+var numTiers = 100,
+    triangle,
+    start,
+    stop;
+
+function pascalRecursive(n, a) {
+
+  if (n < 2) return a;
+
+  var prevTier = a[a.length-1];
+  var curTier = [1];
+
+  for (var i = 1; i < prevTier.length; i++) {
+    curTier[i] = prevTier[i] + prevTier[i-1];
+  }
+  curTier.push(1);
+  a.push(curTier);
+
+  return pascalRecursive(n-1, a);
+}
+
+function pascalSimple(numTiers) {
+
+  var triangle = [
+    [1]
+  ],
+  tier;
+
+  for (var j = 0; j < numTiers-1; j++) {
+    tier = [1];
+    for (var k = 1; k < triangle[j].length; k++) {
+      tier[k] = triangle[j][k] + triangle[j][k-1];
+    }
+    tier.push(1);
+    triangle.push(tier);
+  }
+
+  return triangle;
+}
